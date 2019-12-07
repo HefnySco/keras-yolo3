55be78c24d1fa92601c610853e99163135246e04
mhefny <mohammad.hefny@gmail.com>
2019-12-07 13:55:12 +0200

    adding params to train.py

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ Retrain the YOLO model for your own dataset.
 """
 
 import numpy as np
+import argparse
+
 import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
@@ -12,12 +14,12 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
 
-
+annotation_path = 'train.txt'
+log_dir = 'logs/000/'
+classes_path = 'model_data/voc_classes.txt'
+anchors_path = 'model_data/yolo_anchors.txt'
+model_path = 'model_data/tiny_yolo_weights.h5'                
 def _main():
-    annotation_path = 'train.txt'
-    log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
@@ -27,10 +29,10 @@ def _main():
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+            freeze_body=2, weights_path=model_path)
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path=model_path) # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -186,5 +188,34 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
     if n==0 or batch_size<=0: return None
     return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    parser.add_argument(
+        '--classes_path', type=str,
+        help='path to class definitions, default ' + classes_path
+    )
+
+    parser.add_argument(
+        '--model_path', type=str,
+        help='path to yolo.h5, default ' + model_path
+    )
+
+    parser.add_argument(
+        '--annotation_path', type=str,
+        help='path to train.txt, default ' + annotation_path
+    )
+
+    parser.add_argument(
+        '--anchors_path', type=str,
+        help='path to yolo_anchors.txt, default ' + anchors_path
+    )
+
+
+    FLAGS = parser.parse_args()
+    classes_path = FLAGS.classes_path
+    model_path = FLAGS.model_path
+    annotation_path = FLAGS.annotation_path
+    anchors_path = FLAGS.anchors_path
+
     _main()
